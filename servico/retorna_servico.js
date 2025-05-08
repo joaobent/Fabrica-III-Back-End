4721f0f63c3b02d64ff1b6c3fb9c23ed5261de0c
MariaGoncalves15 <maria.goncalves@estudante.ifro.edu.br>
2025-05-08 10:45:38 -0400

    salvarrrrr

--- servico/retorna_servico.js
+++ servico/retorna_servico.js
@@ -1,1 +1,15 @@
 // Aqui serão colocadas todas as funções para LISTAR(GET) as tabelas 
+
+const db = require('./conexao');
+
+async function retornaFrequencias(req, res) {
+  try {
+    const resultado = await db.query('SELECT * FROM frequencia');
+    res.status(200).json(resultado[0]); // resultado[0] é o array de dados
+  } catch (erro) {
+    console.error('Erro ao retornar frequencias: ', erro);
+    res.status(500).json({ erro: 'Erro interno ao buscar frequências' });
+  }
+}
+
+module.exports = retornaFrequencias;
