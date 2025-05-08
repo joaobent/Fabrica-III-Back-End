c025f1867df6bd96f38c146e69d53f6189cae068
anacaroline16.github.io <caroline.cordeiro@estudante.ifro.edu.br>
2025-05-08 11:10:17 -0300

    teste

--- servico/retorna_servico.js
+++ servico/retorna_servico.js
@@ -1,1 +1,52 @@
 // Aqui serão colocadas todas as funções para LISTAR(GET) as tabelas 
+
+
+
+import pool from "./conexao.js";
+
+export async function retornaFuncionarios() {
+    const conexao = await pool.getConnection();
+    const query = `
+        SELECT 
+            funcionarios.idfuncionarios,
+            funcionarios.nome,
+            funcionarios.senha,
+            funcionarios.cpf,
+            funcionarios.dataDeNascimento,
+            funcionarios.email,
+            funcionarios.telefone,
+            formacao.formacao AS nomeFormacao,
+            formacao.certificado AS nivelFormacao,
+            enderecos.cep,
+            enderecos.numeroCasa,
+            enderecos.complemento
+            
+        FROM funcionarios 
+        INNER JOIN formacao ON funcionarios.formacao_idformacao = formacao.idformacao
+        INNER JOIN enderecos ON funcionarios.endereco_idendereco = enderecos.idendereco
+        ORDER BY funcionarios.nome
+    `;
+    const resultado = await executaQuery(conexao, query);
+    conexao.release();
+    return resultado;
+}
+
+export async function retornaFuncionariosPorNome(nome) {
+    const conexao = await pool.getConnection();
+    const query = `
+        SELECT 
+            funcionarios.nome, funcionarios.telefone, funcionarios.email 
+        FROM funcionarios 
+        WHERE funcionarios.nome LIKE '%${nome}%'
+    `;
+    const resultado = await executaQuery(conexao, query);
+    conexao.release();
+    return resultado;
+}
+
+
+
+async function executaQuery(conexao, query) {
+    const [rows] = await conexao.execute(query);
+    return rows;
+}
