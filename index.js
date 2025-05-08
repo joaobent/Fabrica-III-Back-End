3e0a146a008e6f05b6541af0e0562e0acacdab56
anacaroline16.github.io <caroline.cordeiro@estudante.ifro.edu.br>
2025-05-08 10:47:37 -0300

    ....

--- index.js
+++ index.js
@@ -3,7 +3,34 @@
 
 import express from 'express';
 import pool from './servico/conexao.js';
+import {retornaFuncionarios,
+    retornaFuncionariosPorNome} from "./servico/retorna_servico.js"
 
 const app = express();
 
-app.get
+app.get('/funcionarios', async (req, res) => {
+    let resultado;
+    const nome = req.query.nome;
+
+    try {
+        if (!nome) {
+            resultado = await retornaFuncionarios();
+        } else if (nome) {
+            resultado = await retornaFuncionariosPorNome(nome);
+        } 
+
+        if (resultado.length > 0) {
+            res.json(resultado);
+        } else {
+            res.status(404).json({ mensagem: "Nenhum funcionário encontrado" });
+        }
+    } catch (erro) {
+        console.error("Erro ao buscar funcionários:", erro);
+        res.status(500).json({ mensagem: "Erro interno no servidor" });
+    }
+});
+
+app.listen(20002, () => {
+    const agora = new Date();
+    console.log("Servidor de funcionários rodando em: " + agora);
+});
