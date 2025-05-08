669d44fc67a9b6c78de31cd8799d939e4ea9c6f4
anacaroline16.github.io <caroline.cordeiro@estudante.ifro.edu.br>
2025-05-08 09:43:39 -0300

    conexao ok

---
+++ servico/conexao.js
@@ -0,0 +1,11 @@
+import mysql from 'mysql2/promise';
+
+const pool = mysql.createPool({
+    host     : '200.129.130.149',
+    user     : 'almsfit',
+    password : '12345678',
+    database : 'almsfit_db'
+});
+
+const conexao = await pool.getConnection();
+export default pool;
