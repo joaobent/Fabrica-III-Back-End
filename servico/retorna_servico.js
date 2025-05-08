5472d820e455d30af96a47717a647c0f0beafece
MariaGoncalves15 <maria.goncalves@estudante.ifro.edu.br>
2025-05-08 11:37:10 -0400

    teste_maria

--- servico/retorna_servico.js
+++ servico/retorna_servico.js
@@ -1,8 +1,8 @@
 // Aqui serão colocadas todas as funções para LISTAR(GET) as tabelas 
 
-
-
 import pool from "./conexao.js";
+
+//Funcionários
 
 export async function retornaFuncionarios() {
     const conexao = await pool.getConnection();
@@ -45,6 +45,51 @@ export async function retornaFuncionariosPorNome(nome) {
 }
 
 
+async function executaQuery(conexao, query) {
+    const [rows] = await conexao.execute(query);
+    return rows;
+}
+
+//Frequência
+
+import pool from './conexao.js';
+
+export async function retornaFrequencias() {
+    const conexao = await pool.getConnection();
+
+    const query = `
+        SELECT 
+            idfrequencia,
+            dataEntrada,
+            dataSaida,
+            clientes_idclientes
+        FROM frequencia
+        ORDER BY dataEntrada DESC
+    `;
+
+    const resultado = await executaQuery(conexao, query);
+    conexao.release();
+    return resultado;
+}
+
+export async function retornaFrequenciasPorClienteId(idCliente) {
+    const conexao = await pool.getConnection();
+
+    const query = `
+        SELECT 
+            idfrequencia,
+            dataEntrada,
+            dataSaida,
+            clientes_idclientes
+        FROM frequencia
+        WHERE clientes_idclientes = ?
+        ORDER BY dataEntrada DESC
+    `;
+
+    const [rows] = await conexao.execute(query, [idCliente]);
+    conexao.release();
+    return rows;
+}
 
 async function executaQuery(conexao, query) {
     const [rows] = await conexao.execute(query);
