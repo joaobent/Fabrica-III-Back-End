6eb6c867675be36946d3d39fc99a92c9d7ae71e0
MariaGoncalves15 <maria.goncalves@estudante.ifro.edu.br>
2025-05-11 19:00:01 -0400

    get_frequenciaMaria

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 import express from 'express';
 // import pool from './servico/conexao.js';
 import {retornaFuncionarios,
-    retornaFuncionariosPorNome} from "./servico/retorna_servico.js"
+    retornaFuncionariosPorNome, retornaFrequencias, retornaFrequenciasPorClienteId} from "./servico/retorna_servico.js"
 
 const app = express();
 app.use(express.json()); 
@@ -30,6 +30,37 @@ app.get('/funcionarios', async (req, res) => {
         res.status(500).json({ mensagem: "Erro interno no servidor" });
     }
 });
+
+app.get('/frequencia', async (req, res) => {
+    try {
+        const frequencias = await retornaFrequencias();
+        if (frequencias.length > 0) {
+            res.json(frequencias);
+        } else {
+            res.status(404).json({ mensagem: 'Nenhuma frequência encontrada' });
+        }
+    } catch (erro) {
+        console.error('Erro ao buscar frequências:', erro);
+        res.status(500).json({ mensagem: 'Erro interno no servidor' });
+    }
+});
+
+// Rota para buscar uma frequência por ID
+app.get('/frequencia/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const frequencia = await retornaFrequenciasPorClienteId(id);
+        if (frequencia) {
+            res.json(frequencia);
+        } else {
+            res.status(404).json({ mensagem: 'Frequência não encontrada' });
+        }
+    } catch (erro) {
+        console.error('Erro ao buscar frequência por ID:', erro);
+        res.status(500).json({ mensagem: 'Erro interno no servidor' });
+    }
+});
+
 
 app.listen(9000, () => {
     const data = new Date();
