fa7462f034efe8f1901379602b558142066ed53c
MariaGoncalves15 <maria.goncalves@estudante.ifro.edu.br>
2025-05-11 18:48:38 -0400

    teste_damariateste

--- index.js
+++ index.js
@@ -4,7 +4,8 @@
 import express from 'express';
 // import pool from './servico/conexao.js';
 import {retornaFuncionarios,
-    retornaFuncionariosPorNome} from "./servico/retorna_servico.js"
+    retornaFuncionariosPorNome, retornaFrequencias,
+    retornaFrequenciasPorClienteId} from "./servico/retorna_servico.js"
 
 const app = express();
 
@@ -29,6 +30,51 @@ app.get('/funcionarios', async (req, res) => {
         res.status(500).json({ mensagem: "Erro interno no servidor" });
     }
 });
+
+
+// Rota para buscar todas as frequências
+app.get('/frequencia', async (req, res) => {
+    try {
+        const frequencias = await retornaFrequencias();
+        if (frequencias.length > 0) {
+            res.json(frequencias);
+        } else {
+            res.status(404).json({ mensagem: 'Nenhuma frequência encontrada' });
+        }
+    } catch (erro) {
+        console.error('Erro ao buscar frequências:', erro);
+        res.status(500).json({ mensagem: 'Erro interno no servidor' });
+    }
+});
+
+// Rota para buscar uma frequência por ID
+app.get('/frequencia/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const frequencia = await retornaFrequenciasPorClienteId(id);
+        if (frequencia) {
+            res.json(frequencia);
+        } else {
+            res.status(404).json({ mensagem: 'Frequência não encontrada' });
+        }
+    } catch (erro) {
+        console.error('Erro ao buscar frequência por ID:', erro);
+        res.status(500).json({ mensagem: 'Erro interno no servidor' });
+    }
+});
+
+// Rota para criar uma nova frequência
+app.post('/frequencia', async (req, res) => {
+    const { clientes_idclientes, dataEntrada, dataSaida } = req.body;
+    try {
+        const id = await cadastraFrequencia(clientes_idclientes, dataEntrada, dataSaida);
+        res.status(201).json({ mensagem: 'Frequência criada com sucesso', id });
+    } catch (erro) {
+        console.error('Erro ao criar frequência:', erro);
+        res.status(500).json({ mensagem: 'Erro interno no servidor' });
+    }
+});
+
 
 app.listen(9000, () => {
     const data = new Date();
