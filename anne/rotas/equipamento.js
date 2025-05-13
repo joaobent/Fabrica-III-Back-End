f9a9263356eaa80dc20e4c2722cc650c0ec8c202
hiagogabrielga <hiagogabriel1132@gmail.com>
2025-05-13 18:12:56 -0400

    atualizar

--- anne/rotas/equipamento.js
+++ anne/rotas/equipamento.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { apresentarEquipamentos, apresentarEquipamentosPorId, apresentarEquipamentosPorNome } from '../servicos/EquipamentosServicos/buscar.js';
-import { adicionarEquipamentos } from '../servicos/EquipamentosServicos/adicionar.js';
-import { validarEquipamentos, validarEquipamentosParcial } from '../validacao/EquipamentosValidacao.js';
-import { deletarEquipamentos } from '../servicos/EquipamentosServicos/deletar.js';
-import { editarEquipamentos } from '../servicos/EquipamentosServicos/editar.js';
+import { apresentarEquipamentos, apresentarEquipamentosPorId, apresentarEquipamentosPorNome, apresentarEquipamentosPorIdMarca } from '../servicos/equipamentoServicos/buscar.js';
+import { adicionarEquipamentos } from '../servicos/equipamentoServicos/adicionar.js';
+import { validarEquipamentos, validarEquipamentosParcial } from '../validacao/equipamentoValidacao.js';
+import { deletarEquipamentos } from '../servicos/equipamentoServicos/deletar.js';
+import { editarEquipamentos } from '../servicos/equipamentoServicos/editar.js';
 
 const routerEquipamentos = express.Router();
 
@@ -16,7 +16,7 @@ routerEquipamentos.put('/:id', async (req, res) => {
     }
 
     if (!nome) {
-        return res.status(400).json({ status: 'error', mensagem: 'Nome da Equipamentos é obrigatório' });
+        return res.status(400).json({ status: 'error', mensagem: 'Nome do equipamento é obrigatório' });
     }
 
     const nomeValido = await validarEquipamentosParcial(nome);
@@ -26,25 +26,58 @@ routerEquipamentos.put('/:id', async (req, res) => {
 
     const resultado = await editarEquipamentos(id, nome);
     if (resultado.affectedRows === 0) {
-        return res.status(404).json({ status: 'error', mensagem: 'Equipamentos não encontrada' });
+        return res.status(404).json({ status: 'error', mensagem: 'Equipamentos não encontrado' });
     }
 
     return res.status(200).json({ status: 'success', mensagem: 'Equipamentos editada com sucesso!' });
 });
 
-routerEquipamentos.post('/', async (req, res) => {
-    const { nome } = req.body;
-    if (!nome) {
-        return res.status(400).json({ status: 'error', mensagem: 'Nome da Equipamentos é obrigatório.' });
+routerEquipamentos.patch('/:id', async (req, res) => {
+    const { id } = req.params;
+    const { nome, tipo, numeroDIncricao, descricao, marca_idmarca } = req.body;
+
+    if (isNaN(id)) {
+        return res.status(400).json({ status: 'error', mensagem: 'ID inválido' });
     }
 
-    const nomeValido = await validarEquipamentos(nome);
+    const camposAtualizar = {};
+    if (nome) camposAtualizar.nome_modelo = nome;
+    if (tipo) camposAtualizar.tipo = tipo;
+    if (numeroDIncricao) camposAtualizar.numeroDIncricao = numeroDIncricao;
+    if (descricao) camposAtualizar.descricao = descricao;
+    if (marca_idmarca) camposAtualizar.marca_idmarca = marca_idmarca;
+
+    if (Object.keys(camposAtualizar).length === 0) {
+        return res.status(400).json({ status: 'error', mensagem: 'Nenhum dado fornecido para atualização' });
+    }
+
+    const modeloValido = await validarEquipamentosParcial(nome, tipo, numeroDIncricao, descricao, marca_idmarca);
+    if (!modeloValido.status) {
+        return res.status(400).json({ status: 'error', mensagem: modeloValido.mensagem });
+    }
+
+    const resultado = await editarEquipamentos(id, camposAtualizar);
+    if (resultado.affectedRows === 0) {
+        return res.status(404).json({ status: 'error', mensagem: 'Equipamento não encontrado' });
+    }
+
+    return res.status(200).json({ status: 'success', mensagem: 'Equipamento atualizado com sucesso!' });
+});
+
+
+routerEquipamentos.post('/', async (req, res) => {
+    const { nome, tipo, numeroDIncricao, descricao, marca_idmarca } = req.body;
+    if (!nome || !tipo || !numeroDIncricao || !descricao || !marca_idmarca) {
+        return res.status(400).json({ status: 'error', mensagem: 'Nome da equipamento é obrigatório.' });
+    }
+
+    const nomeValido = await validarEquipamentos(nome, tipo, numeroDIncricao, descricao, marca_idmarca);
     if (!nomeValido.status) {
         return res.status(400).json({ status: 'error', mensagem: nomeValido.mensagem });
     }
 
-    await adicionarEquipamentos(nome);
-    return res.status(201).json({ status: 'success', mensagem: 'Equipamentos cadastrada com sucesso!' });
+    await adicionarEquipamentos(nome, tipo, numeroDIncricao, descricao, marca_idmarca);
+    return res.status(201).json({ status: 'success', mensagem: 'Equipamento cadastrado com sucesso!' });
 });
 
 routerEquipamentos.get('/', async (req, res) => {
@@ -55,7 +88,7 @@ routerEquipamentos.get('/', async (req, res) => {
         : await apresentarEquipamentos();
 
     if (nome && !resultado.length) {
-        return res.status(404).json({ status: 'error', mensagem: 'Equipamentos com esse nome não encontrada' });
+        return res.status(404).json({ status: 'error', mensagem: 'Equipamento com esse nome não encontrado' });
     }
 
     return res.status(200).json({ status: 'success', dados: resultado });
@@ -70,7 +103,7 @@ routerEquipamentos.get('/:id', async (req, res) => {
 
     const resultado = await apresentarEquipamentosPorId(id);
     if (!resultado.length) {
-        return res.status(404).json({ status: 'error', mensagem: 'Equipamentos não encontrada' });
+        return res.status(404).json({ status: 'error', mensagem: 'Equipamento não encontrado' });
     }
 
     return res.status(200).json({ status: 'success', dados: resultado });
@@ -85,10 +118,10 @@ routerEquipamentos.delete('/:id', async (req, res) => {
 
     const resultado = await deletarEquipamentos(id);
     if (resultado.affectedRows === 0) {
-        return res.status(404).json({ status: 'error', mensagem: 'Equipamentos não encontrada' });
+        return res.status(404).json({ status: 'error', mensagem: 'Equipamento não encontrado' });
     }
 
-    return res.status(200).json({ status: 'success', mensagem: 'Equipamentos deletada com sucesso!' });
+    return res.status(200).json({ status: 'success', mensagem: 'Equipamento deletado com sucesso!' });
 });
 
 export default routerEquipamentos;
