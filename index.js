1edb037d6df59e4b62dc271a542c06d348852400
MariaGoncalves15 <maria.goncalves@estudante.ifro.edu.br>
2025-05-18 21:06:26 -0400

    teste documentação

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const swaggerFile = require('./swagger-output.json');
 const app = express();
 app.use(express.json());
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 
 
